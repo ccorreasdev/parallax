3b77e989189eda16cda753b6615ee73d66d306ce
ccorreas <correasberenguer@gmail.com>
2024-03-05 18:18:31 +0100

    Fix parallax mountains

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,14 @@ const clouds_bg = document.querySelector(".clouds_bg")
 
 document.addEventListener("scroll", (e) => {
     let value = window.scrollY
+    console.log(value)
+
+    if (value < 240) {
+        clouds_bg.style.marginBottom = value * 1 + "px";
+    }
 
     title.style.marginTop = value * 1.5 + "px";
     cloud_lonely.style.marginLeft = value * 4 + "px";
-    clouds_bg.style.marginBottom = value * 1 + "px";
+
 
 })
